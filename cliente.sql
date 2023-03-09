42a5e27d79556d59abfd4e254b0d7524a972a45d
jhernandezp <jhernandezp@grupak.com.mx>
2023-03-09 16:20:29 -0600

    update sql

--- cliente.sql
+++ cliente.sql
@@ -14,6 +14,28 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+-- Volcando estructura de base de datos para curso
+DROP DATABASE IF EXISTS `curso`;
+CREATE DATABASE IF NOT EXISTS `curso` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `curso`;
+
+-- Volcando estructura para tabla curso.curso_cliente
+DROP TABLE IF EXISTS `curso_cliente`;
+CREATE TABLE IF NOT EXISTS `curso_cliente` (
+  `cliente_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `cliente_razon_social` varchar(60) DEFAULT NULL,
+  `cliente_email` varchar(200) NOT NULL DEFAULT '',
+  `cliente_estado` char(1) NOT NULL DEFAULT 'A',
+  `cliente_documento` int(10) unsigned NOT NULL,
+  `fk_pais_id` tinyint(4) NOT NULL,
+  `fk_provincia_id` smallint(6) NOT NULL,
+  `fk_location_id` mediumint(9) NOT NULL,
+  PRIMARY KEY (`cliente_id`),
+  UNIQUE KEY `cliente_documento` (`cliente_documento`),
+  KEY `fk_pais_id` (`fk_pais_id`,`fk_provincia_id`,`fk_location_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Tabla de clientes';
+
 -- La exportación de datos fue deseleccionada.
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
